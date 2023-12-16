11baeb8e5840027e8660d347a2ae9e30f133f8ff
Karriss <144677707+Karriss@users.noreply.github.com>
2023-12-16 21:15:48 +0300

    Add files via upload

---
+++ опять.pas
@@ -0,0 +1,76 @@
+﻿program bubblesort;
+
+type
+  tcomparator = function(a, b: integer): boolean;
+
+var
+  inputfilename, outputfilename: string;
+  data: array of integer;
+  i: integer;
+  comparator: tcomparator;
+  inputfile, outputfile: text;
+
+function ascendingcomparator(a, b: integer): boolean;
+begin
+  result := a > b;
+end;
+
+function descendingcomparator(a, b: integer): boolean;
+begin
+  result := a < b;
+end;
+
+procedure bubble(var arr: array of integer; comp: tcomparator);
+var
+  i, j, temp: integer;
+begin
+  // Проходим по всем элементам массива
+  for i := 0 to length(arr) - 1 do
+  begin
+    // Проходим по элементам массива сравнивая каждый соседний элемент
+    for j := 0 to length(arr) - i - 2 do
+    begin
+      // Если текущий элемент больше следующего, меняем их местами
+      if comp(arr[j], arr[j + 1]) then
+      begin
+        temp := arr[j];
+        arr[j] := arr[j + 1];
+        arr[j + 1] := temp;
+      end;
+    end;
+  end;
+end;
+
+begin
+  inputfilename := 'input.txt';
+  outputfilename := 'output.txt';
+
+  // Открываем файл для чтения
+  assign(inputfile, inputfilename);
+  reset(inputfile);
+
+  setlength(data, 0);
+  // Читаем данные из файла
+  while not eof(inputfile) do
+  begin
+    setlength(data, length(data) + 1);
+    readln(inputfile, data[high(data)]);
+  end;
+
+  close(inputfile);
+
+  comparator := ascendingcomparator; // По умолчанию используем компаратор для сортировки по возрастанию
+  bubble(data, comparator); // Сортируем массив с использованием функции bubble
+
+  // Открываем файл для записи
+  assign(outputfile, outputfilename);
+  rewrite(outputfile);
+
+  // Записываем отсортированные данные в файл
+  for i := 0 to length(data) - 1 do
+  begin
+    writeln(outputfile, data[i]);
+  end;
+
+  close(outputfile);
+end.
---
+++ пирамидальная сортировка.pas
@@ -0,0 +1,114 @@
+﻿program HeapSort;
+
+type
+Comparator = function(a, b: Integer): Boolean;
+
+var
+inputFile, outputFile: Text;
+data: array of Integer;
+
+function Greater(a, b: Integer): Boolean;
+begin
+// реализация компаратора для сортировки по убыванию
+Greater := a > b;
+end;
+
+function Less(a, b: Integer): Boolean;
+begin
+// реализация компаратора для сортировки по возрастанию
+Less := a < b;
+end;
+
+procedure Swap(var a, b: Integer);
+var
+temp: Integer;
+begin
+temp := a;
+a := b;
+b := temp;
+end;
+
+procedure Heapify(var arr: array of Integer; n, i: Integer; comp: Comparator);
+var
+largest, l, r: Integer;
+begin
+largest := i;
+l := 2 * i + 1;
+r := 2 * i + 2;
+
+if (l < n) and comp(arr[l], arr[largest]) then
+largest := l;
+
+if (r < n) and comp(arr[r], arr[largest]) then
+largest := r;
+
+if largest <> i then
+begin
+Swap(arr[i], arr[largest]);
+Heapify(arr, n, largest, comp);
+end;
+end;
+
+procedure BuildHeap(var arr: array of Integer; comp: Comparator);
+var
+n, i: Integer;
+begin
+n := Length(arr);
+for i := n div 2 - 1 downto 0 do
+Heapify(arr, n, i, comp);
+end;
+
+procedure Heap(var arr: array of Integer; comp: Comparator);
+var
+n, i: Integer;
+begin
+n := Length(arr);
+
+BuildHeap(arr, comp);
+
+for i := n - 1 downto 0 do
+begin
+Swap(arr[0], arr[i]);
+Heapify(arr, i, 0, comp);
+end;
+end;
+
+procedure ReadData(var arr: array of Integer; var input: Text);
+var
+num, i: Integer;
+begin
+i := 0;
+while not Eof(input) do
+begin
+Read(input, num);
+SetLength(arr, i + 1);
+arr[i] := num;
+Inc(i);
+end;
+end;
+
+procedure WriteData(var arr: array of Integer; var output: Text);
+var
+i: Integer;
+begin
+for i := 0 to Length(arr) - 1 do
+Write(output, arr[i], ' ');
+end;
+
+begin
+Assign(inputFile, 'input.txt');
+Reset(inputFile);
+
+Assign(outputFile, 'output.txt');
+Rewrite(outputFile);
+
+ReadData(data, inputFile);
+
+// вызов пирамидальной сортировки с передачей компаратора в качестве параметра
+Heap(data, Greater);
+
+WriteData(data, outputFile);
+
+Close(inputFile);
+Close(outputFile);
+end.
